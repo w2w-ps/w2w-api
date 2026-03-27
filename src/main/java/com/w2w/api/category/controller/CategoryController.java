f5f34f1c29c2c622efff4b806cf3f00844d439bb
rajarajang-wm <rajarajan.ganesan@wavemaker.com>
2026-03-27 21:51:50 +0530

    feat: add categories API

---
+++ src/main/java/com/w2w/api/category/controller/CategoryController.java
@@ -0,0 +1,26 @@
+package com.w2w.api.category.controller;
+
+import com.w2w.api.category.dto.CategoriesResponse;
+import com.w2w.api.category.dto.CategoryDto;
+import com.w2w.api.category.dto.CategoryGroupDto;
+import com.w2w.api.category.service.CategoryService;
+import com.w2w.api.config.TenantContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/categories")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    @GetMapping
+    public CategoriesResponse getCategories(@RequestParam Integer companyId) {
+        TenantContext.setCurrentTenant(companyId);
+        List<CategoryDto> categories = categoryService.getCategoriesByCompanyId(companyId);
+        List<CategoryGroupDto> groups = categoryService.getCategoryGroupsByCompanyId(companyId);
+        return new CategoriesResponse(categories, groups);
+    }
+}
---
+++ src/main/java/com/w2w/api/category/service/CategoryService.java
@@ -0,0 +1,40 @@
+package com.w2w.api.category.service;
+
+import com.w2w.api.category.dto.CategoryDto;
+import com.w2w.api.category.dto.CategoryGroupDto;
+import com.w2w.api.category.model.Category;
+import com.w2w.api.category.model.CategoryGroup;
+import com.w2w.api.category.repository.CategoryGroupRepository;
+import com.w2w.api.category.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private CategoryGroupRepository categoryGroupRepository;
+
+    public List<CategoryDto> getCategoriesByCompanyId(Integer companyId) {
+        return categoryRepository.findByCompanyId(companyId).stream()
+                .map(cat -> new CategoryDto(cat.getCategoryId(), cat.getDescription(), cat.getShortDesc()))
+                .collect(Collectors.toList());
+    }
+
+    public List<CategoryGroupDto> getCategoryGroupsByCompanyId(Integer companyId) {
+        return categoryGroupRepository.findByCompanyId(companyId).stream()
+                .map(group -> new CategoryGroupDto(
+                        group.getGroupId(),
+                        group.getDescription(),
+                        group.getCategories().stream()
+                                .map(cat -> new CategoryDto(cat.getCategoryId(), cat.getDescription(), cat.getShortDesc()))
+                                .collect(Collectors.toList())
+                ))
+                .collect(Collectors.toList());
+    }
+}
