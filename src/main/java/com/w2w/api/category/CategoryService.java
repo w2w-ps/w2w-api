56c825dc04fcabf3b0efd5620eb056a4bc039f14
rajarajang-wm <rajarajan.ganesan@wavemaker.com>
2026-03-27 23:31:38 +0530

    1. service fixes done

--- src/main/java/com/w2w/api/category/CategoryService.java
+++ src/main/java/com/w2w/api/category/CategoryService.java
@@ -6,11 +6,13 @@ import com.w2w.api.category.repository.CategoryGroupRepository;
 import com.w2w.api.category.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
+@Transactional(readOnly = true)
 public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
