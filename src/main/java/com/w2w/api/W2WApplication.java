ca4e55d2e156be9f325c33df227ea50897438f46
Suresh B <suresh.bhsakar@wavemaker.com>
2026-03-31 14:56:09 +0530

    db pointed to remote

--- src/main/java/com/w2w/api/W2WApplication.java
+++ src/main/java/com/w2w/api/W2WApplication.java
@@ -3,9 +3,12 @@ package com.w2w.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
-public class  W2WApplication {
+public class W2WApplication {
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Kolkata"));
 		SpringApplication.run(W2WApplication.class, args);
 	}
 }
