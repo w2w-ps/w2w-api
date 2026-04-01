6b9d4761a10451795d31a9eed173f944a27c6151
sureshbabu-wm <81563749+sureshbabu-wm@users.noreply.github.com>
2026-04-01 16:01:23 +0530

    Disabled login for APIs temporarily

--- src/main/java/com/w2w/api/config/SecurityConfig.java
+++ src/main/java/com/w2w/api/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login").permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
