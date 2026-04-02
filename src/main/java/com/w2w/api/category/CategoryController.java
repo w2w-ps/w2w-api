151a4547b4e526af3023522c63cf2eff404c2229
rajarajang-wm <rajarajan.ganesan@wavemaker.com>
2026-04-03 00:32:14 +0530

    refactor: rename category and position DTOs

--- src/main/java/com/w2w/api/category/CategoryController.java
+++ src/main/java/com/w2w/api/category/CategoryController.java
@@ -1,8 +1,8 @@
 package com.w2w.api.category;
 
 import com.w2w.api.category.dto.CategoriesResponse;
-import com.w2w.api.category.dto.CategoryDto;
-import com.w2w.api.category.dto.CategoryGroupDto;
+import com.w2w.api.category.dto.CategoryGroupSummary;
+import com.w2w.api.category.dto.CategorySummary;
 import com.w2w.api.config.TenantContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +18,8 @@ public class CategoryController {
     @GetMapping
     public CategoriesResponse getCategories(@RequestParam Integer companyId) {
         TenantContext.setCurrentTenant(companyId);
-        List<CategoryDto> categories = categoryService.getCategoriesByCompanyId(companyId);
-        List<CategoryGroupDto> groups = categoryService.getCategoryGroupsByCompanyId(companyId);
+        List<CategorySummary> categories = categoryService.getCategoriesByCompanyId(companyId);
+        List<CategoryGroupSummary> groups = categoryService.getCategoryGroupsByCompanyId(companyId);
         return new CategoriesResponse(categories, groups);
     }
 }
