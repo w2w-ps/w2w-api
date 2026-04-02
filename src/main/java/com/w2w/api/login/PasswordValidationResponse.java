c5528f975ab27d2cbaa2d36f86b0b1caacf9d1c3
rajarajang-wm <rajarajan.ganesan@wavemaker.com>
2026-04-03 00:32:28 +0530

    refactor: finish login DTO record migration

--- src/main/java/com/w2w/api/login/PasswordValidationResponse.java
+++ src/main/java/com/w2w/api/login/PasswordValidationResponse.java
@@ -2,30 +2,5 @@ package com.w2w.api.login;
 
 import java.util.List;
 
-public class PasswordValidationResponse {
-    private boolean isValid;
-    private List<String> errors;
-    private String message;
-
-    public PasswordValidationResponse() {}
-
-    public PasswordValidationResponse(boolean isValid, List<String> errors) {
-        this.isValid = isValid;
-        this.errors = errors;
-    }
-
-    public PasswordValidationResponse(boolean isValid, List<String> errors, String message) {
-        this.isValid = isValid;
-        this.errors = errors;
-        this.message = message;
-    }
-
-    public boolean getIsValid() { return isValid; }
-    public void setIsValid(boolean isValid) { this.isValid = isValid; }
-
-    public List<String> getErrors() { return errors; }
-    public void setErrors(List<String> errors) { this.errors = errors; }
-
-    public String getMessage() { return message; }
-    public void setMessage(String message) { this.message = message; }
+public record PasswordValidationResponse(boolean isValid, List<String> errors, String message) {
 }
