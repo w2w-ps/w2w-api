b1e4b673ad46dc230ed8d6eb3bc815eb2a815509
Suresh B <suresh.bhsakar@wavemaker.com>
2026-04-06 09:31:43 +0530

    merge issue fix

--- src/main/java/com/w2w/api/config/SecurityConfig.java
+++ src/main/java/com/w2w/api/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/login").permitAll()
+                        .requestMatchers("/api/login/reset-user-account").hasAnyAuthority("Manager")
                         .anyRequest().permitAll())
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
