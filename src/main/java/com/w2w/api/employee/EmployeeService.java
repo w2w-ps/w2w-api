84359da53946592914d98c7bb680a33a5cd99edd
santhosherpa18 <santhosh.erpa@wavemaker.com>
2026-04-09 12:46:42 +0530

    updated comment issues and added getAddManager api changes and removed unsed imports

--- src/main/java/com/w2w/api/employee/EmployeeService.java
+++ src/main/java/com/w2w/api/employee/EmployeeService.java
@@ -105,7 +105,6 @@ public class EmployeeService {
     public void deleteEmployee(Integer id) {
         Employee employee = employeeRepository.findByEmployeeIdAndCompanyIdAndStatusNot(id, CurrentTenant.requireCurrentTenant(), "Deleted")
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Employee not found"));
-
         employee.setStatus("Deleted");
         employeeRepository.save(employee);
     }
