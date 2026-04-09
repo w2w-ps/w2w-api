db49cbd46821a88f865b3d7e788953ecb79572d8
rahul500325 <rahul.gajawda@wavemaker.com>
2026-04-09 17:00:08 +0530

    made changes as per comments

--- src/main/java/com/w2w/api/timeoff/dto/TimeOffRequestSummary.java
+++ src/main/java/com/w2w/api/timeoff/dto/TimeOffRequestSummary.java
@@ -1,5 +1,7 @@
 package com.w2w.api.timeoff.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
@@ -7,11 +9,16 @@ import java.time.LocalTime;
 public record TimeOffRequestSummary(
         Integer requestId,
         Integer employeeId,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate startDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
         LocalDate endDate,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "h:mma")
         LocalTime startTime,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "h:mma")
         LocalTime endTime,
         String endDateTimes,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss")
         LocalDateTime requestedAt,
         String status,
         String comments,
