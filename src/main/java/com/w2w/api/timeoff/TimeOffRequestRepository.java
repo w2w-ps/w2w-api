5212e5e3b962a6735e44fb4e916b0157bea78f5f
rahul500325 <rahul.gajawda@wavemaker.com>
2026-04-09 17:22:37 +0530

    Added cancel request end point

--- src/main/java/com/w2w/api/timeoff/TimeOffRequestRepository.java
+++ src/main/java/com/w2w/api/timeoff/TimeOffRequestRepository.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TimeOffRequestRepository extends JpaRepository<TimeOffRequest, Integer> {
@@ -28,4 +29,6 @@ public interface TimeOffRequestRepository extends JpaRepository<TimeOffRequest, 
             @Param("startDate") LocalDate startDate,
             @Param("endDate") LocalDate endDate
     );
+
+    Optional<TimeOffRequest> findByRequestIdAndCompanyId(Integer requestId, Integer companyId);
 }
