489d33e90f94a34f588dcd9be0741e62c2d006ce
santhosherpa18 <santhosh.erpa@wavemaker.com>
2026-04-16 13:21:26 +0530

    added accessbility columm for employee to get the persmission status

--- src/main/resources/db/migration/V8__add_employee_accessibility_mode_and_unique_email.sql
+++ src/main/resources/db/migration/V8__add_employee_accessibility_mode_and_unique_email.sql
@@ -3,5 +3,5 @@ ALTER TABLE employee ADD COLUMN IF NOT EXISTS accessibility_mode BOOLEAN DEFAULT
 
 -- Add partial unique index on (company_id, email) where email is not null
 CREATE UNIQUE INDEX IF NOT EXISTS idx_employee_company_email
-  ON employee (company_id, email)
-  WHERE email IS NOT NULL AND status != 'Deleted';
+ON employee (company_id, email)
+WHERE email IS NOT NULL AND is_deleted = FALSE;
